421ebdccb3f5a8bdec71e7023cacc12da8452106
Daniel <danielfdoficial@gmail.com>
2023-10-13 11:24:16 -0300

    build: install and use dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => res.send('Hello World!'));
