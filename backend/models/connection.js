4848602827c6a79a93df482f33b1fcbc84d8d7db
Daniel <danielfdoficial@gmail.com>
2023-10-16 20:09:09 -0300

    refactor: delete deprecated arguments on mongodb connection

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,10 +1,5 @@
 const { MongoClient } = require('mongodb');
 
-const OPTIONS = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}
-
 require('dotenv').config();
 const MONGO_DB_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017';
 
@@ -13,7 +8,7 @@ let db = null;
 const connection = () => {
     return db
     ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    : MongoClient.connect(MONGO_DB_URL)
     .then((conn) => {
     db = conn.db('chatTelegramFlashvolve');
     return db;
