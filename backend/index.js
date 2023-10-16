1fb46c24d4ab4c0a1ce721401dd76c7d776bdfa1
Daniel <danielfdoficial@gmail.com>
2023-10-16 11:58:37 -0300

    feat: make token signature auth jwt

--- backend/index.js
+++ backend/index.js
@@ -7,5 +7,12 @@ const PORT = process.env.PORT || 3001;
 const TelegramBot = require('node-telegram-bot-api');
 require('./Integrations/telegramBot')(TelegramBot);
 
+const usersRoute = require('./Routes/usersRoute');
+const loginRoute = require('./Routes/loginRoute');
+
+app.use('/users', usersRoute);
+
+app.use('/login', loginRoute);
+
 app.get('/', (req, res) => res.send('Hello World!'));
 http.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
