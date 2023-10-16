7b712f230ac264e1479f8025a105b84f1894258d
Daniel <danielfdoficial@gmail.com>
2023-10-16 19:28:35 -0300

    fix: empty body in fetch post request

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,10 @@ require('./Integrations/telegramBot')(TelegramBot);
 const usersRoute = require('./Routes/usersRoute');
 const loginRoute = require('./Routes/loginRoute');
 
+app.use(express.json({
+  type: ['application/json']
+})); //https://stackoverflow.com/questions/54016068/empty-body-in-fetch-post-request
+
 app.use('/users', usersRoute);
 
 app.use('/login', loginRoute);
