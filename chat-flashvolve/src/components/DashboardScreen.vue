0bb2d4f49dcf8a31309bdb771795e8b32d59cf7a
Daniel <danielfdoficial@gmail.com>
2023-10-20 14:57:23 -0300

    feat: add dashboard router

---
+++ chat-flashvolve/src/components/DashboardScreen.vue
@@ -0,0 +1,36 @@
+<script>
+// import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    // const newAccount = ref(false);
+    // const errorMessage = ref('');
+    // const username = ref('');
+    // const password = ref('');
+    const router = useRouter();
+
+    const makeLogout = () => {
+      localStorage.removeItem('tokenChatTelegramFlashVolve');
+      return router.push('/');
+    }
+
+    return { makeLogout, router };
+  },
+
+  mounted() {
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h2>Dashboard</h2>
+    <button @click="makeLogout">Sair</button>
+    <div class="dashboard-chats">
+      <form>
+        <input type="text" placeholder="Para iniciar uma conversa, insira aqui um chat.id">
+      </form>
+    </div>
+  </div>
+</template>
--- chat-flashvolve/src/router.js
+++ chat-flashvolve/src/router.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import LoginScreen from './components/LoginScreen'
+import LoginScreen from './components/LoginScreen';
+import DashboardScreen from './components/DashboardScreen';
+
 
 const routes = [
   { path: '/', component: LoginScreen },
+  { path: '/dashboard', component: DashboardScreen },
 ];
 
 const router = createRouter({
