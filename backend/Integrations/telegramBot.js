c3350f7949d70d6e4fedb7b6ff84abdd290218d9
Daniel <danielfdoficial@gmail.com>
2023-10-24 11:47:52 -0300

    refactor: remove unused code

--- backend/Integrations/telegramBot.js
+++ backend/Integrations/telegramBot.js
@@ -1,23 +1,9 @@
 require('dotenv').config();
 const messagesController = require("../controllers/messagesController");
 
-module.exports = (TelegramBot, io) => {
-  const token = `${process.env.BOT_TELEGRAM_TOKEN}`;
-  const bot = new TelegramBot(token, {polling: true});
-  
-  bot.onText(/\/echo (.+)/, (msg, match) => {  
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-    bot.sendMessage(chatId, resp);
-  });
-
+module.exports = (bot, io) => {
   bot.on('message', async (msg) => {
-    console.log(msg)
-    const chatId = msg.chat.id;
-
     io.emit('telegramMessage', msg);
-    bot.sendMessage(chatId, 'Received your message!!');
-
     try {
       const requisition = { 
         body: { source: 'telegram', chat: msg.chat, date: msg.date, text: msg.text }
